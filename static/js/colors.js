5ab7eaf79085a766c1480922435b8c06e536edfb
Samuel de leon <samueldeleonglaz@gmail.com>
2025-04-22 13:10:55 +0100

    Add custom color palette for Neurolimpiadas using Tailwind CSS

---
+++ static/js/colors.js
@@ -0,0 +1,32 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                neurolimpiadas: {
+                    blue: '#4672b5',
+                    bluehover: '#3a5d94',
+                    bluepalido: '#e8f0fd',
+               
+                    yellow: '#e6ba38',
+                    yellowHover: '#c59f33',
+                    yellowpalido: '#fffbeb',
+
+                    lightBlue: '#75addb',
+                    lightBlueHover: '#5f8bb0',
+
+
+                    red: "#b1235c",
+                    redHover: "#8e1c4a",
+                    redpalido: "#fde8f1",
+
+
+                    green: '#86b050',
+                    greenHover: '#6b8d40',
+                    greenpalido: '#f5fee9',
+                }
+            }
+        }
+    }
+}
+
+
